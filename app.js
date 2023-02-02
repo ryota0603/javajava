d0dc0f6e525efcb157b7206e2a88c19ecdd63cf2
ryota0603 <114867300+ryota0603@users.noreply.github.com>
2023-02-02 08:40:00 +0000

    js

---
+++ app.js
@@ -0,0 +1,45 @@
+// let hello ='Hello Would';
+// alert(hello);
+// alert('hello' + 'would');
+// let str1 = 'Hello';
+// let str2 = 'World!!';
+// alert(str1 + str2);
+// let orange = 100;
+// let apple = 120;
+
+// if(orange < apple){
+//   alert('みかんの値段がりんごより安い');
+// } else if(orange == apple){
+//   alert('みかんとりんごが同じ値段');
+// } else{
+//   alert('みかんの値段がりんごより高い');
+// }
+// let max = 100;
+// let num = 1;
+// let count = 0;
+
+// while(num < max){
+//   num = num * 2;
+//   count = count + 1;
+// }
+
+// alert('2を掛け続けて' + max + 'を超えるのに必要だった回数は' + count + '回です');
+let i;
+let num = 0;
+
+for(i = 1; i < 11; i++){
+  num = num + i;
+}
+
+alert('1から10まで足し算した結果は' + num + 'です');
+// 足し算
+alert(4 + 3);
+
+// 引き算
+alert(8 - 5);
+
+// 掛け算
+alert(2 * 6);
+
+// 割り算
+alert(10 / 2);
