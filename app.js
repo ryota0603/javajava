035b913dd6b13ae6ba5da373085186e664b87698
ryota0603 <114867300+ryota0603@users.noreply.github.com>
2023-02-04 16:22:48 +0000

    java

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
-let hello ='Hello Would';
-alert(hello);
+// let hello ='Hello Would';
+// alert(hello);
 // alert('hello' + 'would');
 // let str1 = 'Hello';
 // let str2 = 'World!!';
 // alert(str1 + str2);
-// let orange = 100;
-// let apple = 120;
+let orange = 100;
+let apple = 120;
 
-// if(orange < apple){
-//   alert('みかんの値段がりんごより安い');
-// } else if(orange == apple){
-//   alert('みかんとりんごが同じ値段');
-// } else{
-//   alert('みかんの値段がりんごより高い');
-// }
+if(orange < apple){
+  alert('みかんの値段がりんごより安い');
+} else if(orange == apple){
+  alert('みかんとりんごが同じ値段');
+} else{
+  alert('みかんの値段がりんごより高い');
+}
 // let max = 100;
 // let num = 1;
 // let count = 0;
